85e66dfd154d3f9582e9f18c5169fab76db8ae68
Parham Alvani <parham.alvani@gmail.com>
2016-07-17 16:34:09 +0430

    Led sample code was added.

---
+++ example/led.py
@@ -0,0 +1,17 @@
+# In The Name Of God
+# ========================================
+# [] File Name : led.py
+#
+# [] Creation Date : 17-07-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+import RPi.GPIO as GPIO
+
+# Setup library configuration
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Trun GPIO15 on
+GPIO.setup(15, GPIO.OUT)
+GPIO.output(15, True)
