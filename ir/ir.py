97f1f5d01c7b4588862df8e06b81bd2c501b33ac
Parham Alvani <parham.alvani@gmail.com>
2016-07-24 14:17:48 +0430

    IR project was improved.

--- ir/ir.py
+++ ir/ir.py
@@ -8,6 +8,7 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 import RPi.GPIO as GPIO
+import time
 
 # Setup library configuration
 GPIO.setmode(GPIO.BCM)
@@ -15,13 +16,14 @@ GPIO.setwarnings(False)
 
 # Trun GPIO15 on
 GPIO.setup(15, GPIO.IN)
+t = 0
 try:
     while True:
-        try:
-            GPIO.wait_for_edge(15, GPIO.RISING)
-        except RuntimeError:
-            break
-        for i in range(0, 4):
-            print(GPIO.input(15))
+            if(GPIO.input(15)):
+                t += 0.5
+            else:
+                print(t)
+                t = 0
+            time.sleep(0.5)
 finally:
     print("Thank you for using 18.20 :)")
